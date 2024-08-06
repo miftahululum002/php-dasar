ab471bdaee60f7c22a8a9b3639c82792d14576dc
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-06 16:03:57 +0700

    ll

---
+++ latihan/array/praktik/1.php
@@ -0,0 +1,14 @@
+<?php
+$buku = array();
+$baju = [];
+//membuat array sekaligus diisi
+$bunga = array("matahari", "dandelion", "mawar");
+$hewan = ["singa", "kuda", "anjing"];
+//membuat array dan mengisi indeks tertentu
+$anggota[1] = "Dinda";
+$anggota[2] = "Bina";
+//menampilkan array
+echo $baju . "<br>";
+echo $bunga[0] . "<br>";
+echo $bunga[1] . "<br>";
+echo $anggota[2] . "<br>";
